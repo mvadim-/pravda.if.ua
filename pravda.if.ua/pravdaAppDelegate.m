f9e1e5fa5ee9fa5eb40ba0da7e38fcf5a75c0c0e
mvadim- <mvadim@me.com>
2014-04-12 11:23:43 +0300

    check internet connection before start update data in background

--- pravda.if.ua/pravdaAppDelegate.m
+++ pravda.if.ua/pravdaAppDelegate.m
@@ -10,38 +10,33 @@
 #import "pravdaViewController.h"
 #import "API.h"
 #import "RSSItem.h"
+#import "AFNetworkReachabilityManager.h"
+
 @implementation pravdaAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     NSLog(@"Launched in background %d", UIApplicationStateBackground == application.applicationState);
-
     // Override point for customization after application launch.
     return YES;
 }
 
 -(void)application:(UIApplication *)application performFetchWithCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 {
-    NSLog(@"Fetch started");
-    NSDate *lastUpdateSavedTime = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastUpdateTime"];
-    __block NSDate *lastUpdateTime;
-    
-    [[API sharedInstance] refreshDataFromServerWithCategory:nil andOffset:nil completionBlock:^(NSArray *response, bool succeeded, NSError *error) {
-        if (succeeded) {
-            lastUpdateTime = [(RSSItem *)[response firstObject] pubDate];
-            NSLog(@"lastUpdateSavedTime:%@, lastUpdateTime:%@",lastUpdateSavedTime,lastUpdateTime);
-            if ([lastUpdateSavedTime compare: lastUpdateTime] != NSOrderedSame) {
-                NSLog(@"Application launched in background");
-                [UIApplication sharedApplication].applicationIconBadgeNumber = 1;
-                completionHandler(UIBackgroundFetchResultNewData);
-
-            }else  completionHandler(UIBackgroundFetchResultNoData);
-
-        }
-    }];
-    // Set up Local Notifications
-    
-    NSLog(@"Fetch completed");
+    if ([[AFNetworkReachabilityManager sharedManager] isReachable])
+    {
+        [[API sharedInstance] refreshDataFromServerWithCategory:nil andOffset:nil completionBlock:^(NSArray *response, bool succeeded, NSError *error) {
+            if (succeeded) {
+                NSDate *lastUpdateSavedTime = [[NSUserDefaults standardUserDefaults] objectForKey:@"lastUpdateTime"];
+                NSDate *lastUpdateTime = [(RSSItem *)[response firstObject] pubDate];
+                if ([lastUpdateSavedTime compare: lastUpdateTime] != NSOrderedSame) {
+                    NSLog(@"Application updated in background");
+                    [UIApplication sharedApplication].applicationIconBadgeNumber = 1;
+                    completionHandler(UIBackgroundFetchResultNewData);
+                }else  completionHandler(UIBackgroundFetchResultNoData);
+            }
+        }];
+    }
 }
 
 -(BOOL)application:(UIApplication *)application willFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -58,20 +53,20 @@
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
     // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-
+    
     // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
     [UIApplication sharedApplication].applicationIconBadgeNumber = 0;
-
+    
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
